7eac1e830b5486e84fef8d5b4a2905a16d87c7a8
Meira Pentermann <meira@meirapentermann.com>
2017-10-22 14:00:40 -0600

    dao methods complete, start controller

--- PetRest/src/controllers/PetSitController.java
+++ PetRest/src/controllers/PetSitController.java
@@ -1,18 +1,44 @@
 package controllers;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import data.PetDAO;
+import entities.Pet;
+
 @RestController
 public class PetSitController {
 	
-//	@Autowired
-//	PetDAO petDao;
+	@Autowired
+	PetDAO petDao;
 	
 	@RequestMapping(path="ping", method=RequestMethod.GET)
 	public String ping() {
 		return "pong";
+	}
+	
+	@RequestMapping(path="pets", method=RequestMethod.GET)
+	public List<Pet> index(HttpServletResponse res) {
+		List<Pet> list = petDao.getAll();
+		if(list.size() > 0) {
+			res.setStatus(200);
+		}
+		else {
+			res.setStatus(204);
+		}
+		return list;
+	}
+	
+	@RequestMapping(path="pets/{id}", method=RequestMethod.GET)
+	public Pet show(@PathVariable int id, HttpServletResponse res) {
+		
 	}
 
 }
--- PetRest/src/data/PetDAOImpl.java
+++ PetRest/src/data/PetDAOImpl.java
@@ -2,43 +2,81 @@ package data;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import entities.Pet;
 
 @Transactional
 @Repository
 public class PetDAOImpl implements PetDAO {
+	
+	@PersistenceContext
+	private EntityManager em;
 
 	@Override
 	public List<Pet> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+		String queryString = "Select p from pet";
+		return em.createQuery(queryString, Pet.class)
+				 .getResultList();
 	}
 
 	@Override
 	public Pet getById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		return em.find(Pet.class, id);
 	}
 
 	@Override
 	public Pet createNew(String petJSON) {
-		// TODO Auto-generated method stub
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			Pet mappedPet = mapper.readValue(petJSON, Pet.class);
+			em.persist(mappedPet);
+			em.flush();
+			return mappedPet;
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
 	@Override
 	public Pet updatePet(int id, Pet pet) {
-		// TODO Auto-generated method stub
-		return null;
+		Pet managedPet = em.find(Pet.class, id);
+		if(managedPet != null) {
+			if(pet.getFamilyName() != null && pet.getFamilyName() != "") {
+				managedPet.setFamilyName(pet.getFamilyName());
+			}
+			if(pet.getTasks() != null && pet.getTasks() != "") {
+				managedPet.setTasks(pet.getTasks());
+			}
+			if(pet.getDailyRate() != 0) {
+				managedPet.setDailyRate(pet.getDailyRate());
+			}
+			if(pet.getNumDays() != 0) {
+				managedPet.setDailyRate(pet.getDailyRate());
+			}
+			if(pet.getPetName() != null && pet.getPetName() != "") {
+				managedPet.setPetName(pet.getPetName());
+			}
+		}
+		return pet;
 	}
 
 	@Override
 	public boolean destroy(int id) {
-		// TODO Auto-generated method stub
-		return false;
+		Pet managedPet = em.find(Pet.class, id);
+		if(managedPet==null) {
+			return false;
+		}
+		em.remove(managedPet);
+		return true;
 	}
 	
 
