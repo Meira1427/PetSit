0697a6bb7f486f429235e73d80328bcec1571c2e
Meira Pentermann <meira@meirapentermann.com>
2017-10-27 14:55:05 -0600

    link up component, start service

---
+++ PetRest/WebContent/app/appModule/petsit.service.js
@@ -0,0 +1,7 @@
+angular.module('petsitModule')
+	.factory('petsitService', function($http, $filter){
+		var service = {};
+		
+		
+		return service;
+	});
---
+++ PetRest/WebContent/app/appModule/petsit/petsit.component.js
@@ -0,0 +1,16 @@
+angular.module('petsitModule')
+	.component('petsit', {
+		templateUrl: 'app/appModule/petsit/petsit.component.html',
+		
+		controller: function(){
+			var vm = this;
+			
+			
+			
+			
+			
+			
+		},
+			
+		controllerAs: 'vm'
+	});
