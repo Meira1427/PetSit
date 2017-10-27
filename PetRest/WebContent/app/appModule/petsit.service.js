595f1c74a42b8dee3463796339c006bfc0be6206
Meira Pentermann <meira@meirapentermann.com>
2017-10-27 15:23:06 -0600

    added list and goto edit function

--- PetRest/WebContent/app/appModule/petsit.service.js
+++ PetRest/WebContent/app/appModule/petsit.service.js
@@ -2,6 +2,13 @@ angular.module('petsitModule')
 	.factory('petsitService', function($http, $filter){
 		var service = {};
 		
+		service.index = function(){
+			return $http({
+				method : 'GET',
+				url : 'api/pets'
+			})	
+		};
+		
 		
 		return service;
 	});
--- PetRest/WebContent/app/appModule/petsit/petsit.component.js
+++ PetRest/WebContent/app/appModule/petsit/petsit.component.js
@@ -2,10 +2,35 @@ angular.module('petsitModule')
 	.component('petsit', {
 		templateUrl: 'app/appModule/petsit/petsit.component.html',
 		
-		controller: function(){
+		controller: function(petsitService, $filter){
 			var vm = this;
 			
+			vm.selected = null;
 			
+			
+			vm.petlist = [];
+			
+			var getAll = function(){
+				petsitService.index()
+				.then(function(response){
+					vm.petlist = response.data;
+					console.log(vm.petlist);
+				})
+				.catch(function(error){
+					console.log(error)
+				});
+			};
+			
+			getAll();
+			
+			vm.displayPet = function(pet){
+				vm.selected = pet;
+				console.log(vm.selected);
+			};
+			
+			vm.displayTable = function() {
+				vm.selected = null;
+			}
 			
 			
 			
